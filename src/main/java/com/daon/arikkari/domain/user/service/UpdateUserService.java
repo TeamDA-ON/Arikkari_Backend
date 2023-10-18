b169b694693fa45944f7e2ee58d12cb2a3d4ca02
NameIsUser06 <woosoungcho@gmail.com>
2023-10-19 02:01:17 +0900

    FIX : return

--- src/main/java/com/daon/arikkari/domain/user/service/UpdateUserService.java
+++ src/main/java/com/daon/arikkari/domain/user/service/UpdateUserService.java
@@ -22,6 +22,6 @@ public class UpdateUserService {
         user.setName(request.getName());
         user.setBelong(request.getBelong());
         userRepository.save(user);
-        ResponseEntity.ok("succcess");
+        return ResponseEntity.ok("succcess");
     }
 }
